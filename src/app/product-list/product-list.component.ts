b820850d4b30856a483a6f333e27988b4da140e8
fdepedis <flavio.depedis@gmail.com>
2020-06-09 17:15:32 +0200

    Aggiunto ciclo for in list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,11 +11,11 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert("The product has been shared!");
+    window.alert("Flax....he product has been shared!");
   }
 
   onNotify() {
-    window.alert("You will be notified when the product goes on sale");
+    window.alert("Flax...You will be notified when the product goes on sale");
   }
 }
 
